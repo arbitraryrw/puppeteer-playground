230d6333c6b36e692d1f7342d8fa49157d7db008
arbitraryrw <nickcucakovic@gmail.com>
2021-01-22 20:42:22 +0000

    PoC logic introduced for pulling headings from hacker news links

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 console.log("*** Puppeteer Playground ***");
 
+const tools = require('./common/getLinks.js');
+
+
+console.log(tools.foo());
+
+
 const puppeteer = require('puppeteer');
 const url = process.argv[2];
 if (!url) {
     throw "Please provide URL as a first argument";
 }
-
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -24,6 +29,20 @@ if (!url) {
 //   await page.setDefaultNavigationTimeout(5000); 
   await page.goto(url);
   await page.screenshot({path: 'example.png'});
+
+
+  const hackerNews = await browser.newPage()
+  await hackerNews.tracing.start({
+    path: 'trace.json',
+    categories: ['devtools.timeline']
+  })
+  await hackerNews.goto('https://news.ycombinator.com/news')
+
+  // execute standard javascript in the context of the hackerNews.
+  const stories = await hackerNews.$$eval('a.storylink', anchors => { return anchors.map(anchor => anchor.textContent).slice(0, 10) })
+  console.log(stories)
+  await hackerNews.tracing.stop()
+
 
   await browser.close();
 })();
